59b4e2ac6fd63f69e0aada0556bd2f3f843d4e46
MaralPirveisi <maralpirvaisi@gmail.com>
2026-05-12 19:06:12 +0330

    Add even/odd number checker

---
+++ Conditions/1.py
@@ -0,0 +1,8 @@
+number=int(input("please enter your number:"))
+if number>0:
+    if number % 2 ==0:
+        print("your number is even")
+    else:
+        print("your number is odd")
+else:
+    print("please enter a number bigger than 0")
