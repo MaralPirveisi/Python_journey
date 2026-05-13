a870f8075c12d9beb81f1e568edec2e8c995afcc
MaralPirveisi <maralpirvaisi@gmail.com>
2026-05-13 17:29:41 +0330

    Create login_system.py

---
+++ Conditions/login_system.py
@@ -0,0 +1,8 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "1234":
+    print("Welcome!")
+
+else:
+    print("Access denied")
